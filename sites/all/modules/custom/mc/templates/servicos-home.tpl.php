c00d22d8c949f30263948c5b42981154189010d1
Miguel Trindade <migueltrindadejr@gmail.com>
2013-09-26 19:24:50 -0300

    servicos home

--- sites/all/modules/custom/mc/templates/servicos-home.tpl.php
+++ sites/all/modules/custom/mc/templates/servicos-home.tpl.php
@@ -6,10 +6,11 @@
     $direcao = ($count >= 3 ? 'left' : 'right');
     ?>
     <div class="col-md-2 servicos-item clearfix">
-      <div class="icone">
-        <a class="botao" title="<?php print $item['title']; ?>" data-content="<?php print $item['descricao']; ?>" <?php print drupal_attributes($item['attributes']); ?> data-placement="<?php print $direcao; ?>" data-toggle="popover">
-          <span><?php print $item['title']; ?></span>
-        </a>
+      <div class="col-sm-4 col-md-12 col-lg-12 icone">
+        <?php print $item['imagem']; ?>
+<!--        <a class="botao" title="--><?php //print $item['title']; ?><!--" data-content="--><?php //print $item['descricao']; ?><!--" --><?php //print drupal_attributes($item['attributes']); ?><!-- data-placement="--><?php //print $direcao; ?><!--" data-toggle="popover">-->
+<!--          <span>--><?php //print $item['title']; ?><!--</span>-->
+<!--        </a>-->
       </div>
       <div class="hidden-lg hidden-md col-sm-8">
         <h2><?php print $item['title']; ?></h2>
@@ -23,8 +24,11 @@
     $count++;
   endforeach;
   ?>
+
 </div>
-<div class="center hidden-md hidden-lg">
-  <a href="#" id="prev"> << Prev </a>
-  <a href="#" id="next"> Next >> </a>
-</div>
+
+<div class="cycle-pager"></div>
+<!--<div id="servicos-cycle-pager" class="cycle-pager hidden-md hidden-lg">-->
+<!--  <a href="#" id="prev"> << Prev </a>-->
+<!--  <a href="#" id="next"> Next >> </a>-->
+<!--</div>-->
--- sites/all/themes/mistura/js/main.js
+++ sites/all/themes/mistura/js/main.js
@@ -8,52 +8,111 @@
         $(this).removeClass('animated flipInX ')
       });
 
-      $('body.front .servicos [data-toggle="popover"]').popover({
-        trigger: 'click',
-        html: true
-      }).on('show.bs.popover hide.bs.popover', function () {
-          //$(this).toggleClass('active');
-      });
-
-//      if (matchMedia('all and (max-width: 480px)').matches) {
-//        console.log('Mobile');
-//      }
-//      else if (matchMedia('all and (max-width: 768px)').matches) {
-//        console.log('Tablet');
-//      }
-//      else if (matchMedia('all and (max-width: 992px)').matches) {
-//        console.log('Monitor');
-//      }
-//      else if (matchMedia('all and (min-width: 992px)').matches) {
-//        console.log('TV e maiores');
-//      }
-
-
-      mql('all and (max-width: 480px)', mobile_js);
-      mql('all and (max-width: 768px)', mobile_js);
-      mql('all and (min-width: 992px)', desktop_js);
-      mql('all and (min-width: 1200px)', desktop_js);
-
-      function desktop_js(mql) {
-        $('.servicos .row').cycle('destroy');
+      if (matchMedia('all and (max-width: 480px)').matches) {
+        console.log('Mobile');
+        servicos_cycle();
+      }
+      else if (matchMedia('all and (max-width: 768px)').matches) {
+        console.log('Mobile / Tablet');
+        servicos_cycle();
+      }
+      else if (matchMedia('all and (min-width: 768px) and (max-width: 992px)').matches) {
+        console.log('Tablet');
+        servicos_cycle();
+      }
+      else if (matchMedia('all and (min-width: 992px) and (max-width: 1200px)').matches) {
+        console.log('Desktop');
+        servicos_popover();
+      }
+      else if (matchMedia('all and (min-width: 1200px)').matches) {
+        console.log('Tv e Maiores');
+        servicos_popover();
       }
 
 
-      function mobile_js (mql) {
-        if (mql.matches) {
-          $('.servicos .row').cycle({
-            prev: '#prev',
-            next: '#next',
-            slides: '> div'
-          });
-        }
+      //mql('all and (max-width: 480px)', mobile_js);
+      //mql('all and (max-width: 768px)', mobile_js);
+      //mql('all and (min-width: 992px)', desktop_js);
+      //mql('all and (min-width: 1200px)', desktop_js);
+
+//      function desktop_js(mql) {
+//        $('.servicos .row').cycle('destroy');
+//      }
+//      function mobile_js (mql) {
+//        if (mql.matches) {
+//          servicos_cycle();
+//        }
+//      }
+
+      // Serviços
+      function servicos_cycle() {
+        $('.servicos .row').cycle({
+          slides: '> .servicos-item',
+          pager: '.cycle-pager'
+        });
       }
+
+      function servicos_popover() {
+        $('.servicos [data-toggle="popover"]').popover({
+          trigger: 'hover',
+          html: true,
+          pager: '#servicos-cycle-pager'
+        });
+      }
+      // Serviços
+
     }
 	}
 
 }(jQuery));
 
 
+/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license */
+
+window.matchMedia || (window.matchMedia = function() {
+  "use strict";
+
+  // For browsers that support matchMedium api such as IE 9 and webkit
+  var styleMedia = (window.styleMedia || window.media);
+
+  // For those that don't support matchMedium
+  if (!styleMedia) {
+    var style = document.createElement('style'),
+      script = document.getElementsByTagName('script')[0],
+      info = null;
+
+    style.type = 'text/css';
+    style.id = 'matchmediajs-test';
+
+    script.parentNode.insertBefore(style, script);
+
+    // 'style.currentStyle' is used by IE <= 8 and 'window.getComputedStyle' for all other browsers
+    info = ('getComputedStyle' in window) && window.getComputedStyle(style) || style.currentStyle;
+
+    styleMedia = {
+      matchMedium: function(media) {
+        var text = '@media ' + media + '{ #matchmediajs-test { width: 1px; } }';
+
+        // 'style.styleSheet' is used by IE <= 8 and 'style.textContent' for all other browsers
+        if (style.styleSheet) {
+          style.styleSheet.cssText = text;
+        } else {
+          style.textContent = text;
+        }
+
+        // Test if media query is true or false
+        return info.width === '1px';
+      }
+    };
+  }
+
+  return function(media) {
+    return {
+      matches: styleMedia.matchMedium(media || 'all'),
+      media: media || 'all'
+    };
+  };
+}());
 
 
 mql = (function(doc, undefined){
